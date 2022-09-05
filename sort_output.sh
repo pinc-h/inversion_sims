45f69a682577fda2812050d31d59b32eb915bc5a
alexpinch <alexpinch@users.noreply.github.com>
2022-09-04 17:18:38 -0700

    Compresses output into a dated zip

--- sort_output.sh
+++ sort_output.sh
@@ -13,4 +13,11 @@ for i in {1..100}; do
 		cd ..
 	fi
 done
-cd ..
+DIR=$(date +%Y%m%d%H%M)
+mkdir "${DIR}"
+mv full_runs ${DIR}/full_runs
+mv lost_runs ${DIR}/lost_runs
+ls | time parallel -j 2 tar -zcvf ${DIR}.tar.gz ${DIR}
+rm -r ${DIR}
+
+
