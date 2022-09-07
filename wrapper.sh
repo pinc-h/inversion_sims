00b92e4cbd2fe7ead8b24263fcb08464b90742ef
alexpinch <alexpinch@hotmail.com>
2022-09-07 12:49:58 -0700

    New wrapper.sh, runs run_model.sh and sort_output.sh

--- wrapper.sh
+++ wrapper.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
-i=$(ls -l data/ | wc -l)
-cd data
-mkdir -p ${i}
-cd ${i}
-slim ../../model.slim
-# These awk commands replace all spaces with commas
-for file in *.csv; do
-	awk -F"," '{ gsub(/ /, ","); }1' "${file}" > "parsed_${file}"
-done
-for file in *.*; do
-	mv ${file} ${i}_${file}
-done
+seq 100 | parallel -j 2 bash run_model.sh
+bash sort_output.sh
+say "Beep"
