484c1941ab49cd430c0edfaa9aee0b0479adc05e
alexpinch <alexpinch@hotmail.com>
2022-09-09 17:06:12 -0700

    Update wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -1,3 +1,5 @@
-seq 100 | parallel -j 2 bash run_model.sh
-bash sort_output.sh
-say "Beep"
+#!/bin/bash
+read -p "Enter number of runs: " runs
+seq $runs | parallel -j 2 bash run_model.sh
+bash sort_output.sh $runs
+echo "Complete"
