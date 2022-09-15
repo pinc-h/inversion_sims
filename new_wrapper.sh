864fc9e1b18a80b7189984773f884d1d21a95d57
alexpinch <alexpinch@hotmail.com>
2022-09-15 12:33:30 -0700

    Update new_wrapper.sh

--- new_wrapper.sh
+++ new_wrapper.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-i=$1
-j=$1
+i=1
+j=1
 
-while j < 100; do
+while [ ${j} -le 100 ]
+do
 	seq 1 | parallel -j 1 bash run_model.sh
 	if [ -f ${i}/"${i}_lost_fitness.csv" ]; then
+		echo "${i} runs"
+		echo "${j} runs lost"
+
         else
-		j = j + 1
+		((j++))
         fi 
-	i = i + 1
+	((i++))
 done
-bash sort_output.sh $i
+bash sort_output.sh ${i}
