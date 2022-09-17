e943a5e0f148a19078826e0c6a3454058875b609
alexpinch <alexpinch@hotmail.com>
2022-09-16 20:27:40 -0700

    Update sort_output.sh

--- sort_output.sh
+++ sort_output.sh
@@ -18,7 +18,9 @@ for i in $(seq $runs); do
 done
 DIR=$(date +%Y%m%d%H%M)
 mkdir "${DIR}"
-mv full_runs ${DIR}/full_runs
-mv lost_runs ${DIR}/lost_runs
+cp full_runs ${DIR}/full_runs
+cp lost_runs ${DIR}/lost_runs
 tar --use-compress-program="pigz -k -p2" -cf ${DIR}.tar.gz ${DIR}
 # -p is the number of processors to use
+rm -r "${DIR}"
+mv ${DIR}.tar.gz ../raw_output/${DIR}.tar.gz
