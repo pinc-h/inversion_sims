cbd4a24b5c971fd6d8032387660e5dddd4e2fd33
alexpinch <alexpinch@hotmail.com>
2022-09-20 14:27:56 -0700

    Update run_model.sh

--- run_model.sh
+++ run_model.sh
@@ -4,6 +4,7 @@ cd data
 mkdir -p ${i}
 cd ${i}
 slim ../../model.slim
+#if statement on if the inversion is lost, then run again
 # These awk commands replace all spaces with commas
 for file in *.csv; do
 	awk -F"," '{ gsub(/ /, ","); }1' "${file}" > "parsed_${file}"
