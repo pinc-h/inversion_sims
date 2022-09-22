4122b55c6e6a7e6b863abe28f732dcd18fa852b9
alexpinch <alexpinch@hotmail.com>
2022-09-22 16:26:44 -0700

    Delete average_fit.R

--- processing_scripts/average_fit.R
+++
@@ -1,5 +0,0 @@
-library(tidyverse)
-setwd("/Users/alexpinch/Documents/GitHub/inversion-model")
-
-# Calculating averages and what not from the single large all_data.csv
-# Save smaller CSVs with the averages to /processed
