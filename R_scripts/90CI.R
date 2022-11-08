3eebe5f563cead87cedff813279dc2d726f32f1d
alexpinch <alexpinch@hotmail.com>
2022-11-07 22:41:01 -0800

    Update 90CI.R

--- R_scripts/90CI.R
+++ R_scripts/90CI.R
@@ -1,13 +1,13 @@
 library(tidyverse)
-setwd("/Users/alexpinch/Documents/GitHub/inversion-model")
+setwd("/Users/alexpinch/Documents/GitHub/inversion_model")
 
 all_data <- tibble()
-files <- length(list.files("/Users/alexpinch/Documents/GitHub/inversion-model/data/full_runs"))
-full_runs <- list.files("/Users/alexpinch/Documents/GitHub/inversion-model/data/full_runs")
+files <- length(list.files("/Users/alexpinch/Documents/GitHub/inversion_model/oct15_data/full_runs"))
+full_runs <- list.files("/Users/alexpinch/Documents/GitHub/inversion_model/oct15_data/full_runs")
 for (i in 1:files) {
   run <- (full_runs[i])
   typeof(run)
-  setwd(file.path("/Users/alexpinch/Documents/GitHub/inversion-model/data/full_runs/",run))
+  setwd(file.path("/Users/alexpinch/Documents/GitHub/inversion_model/oct15_data/full_runs/",run))
   run_data <- read.csv(file = paste(run,".csv",sep=""),skip=1,header=F) %>%
     rename(gen=V1,pop=V2,sample=V3,fitness=V4,inv_genotype=V5)
   run_data <- run_data %>% mutate(sim_run=run)
@@ -72,7 +72,7 @@ summarized_data <- all_data %>%
   group_by(gen,inv_genotype) %>%
   mutate(qnt_90 = quantile(mean_fit, 0.9),
          qnt_10 = quantile(mean_fit, 0.1),
-         mean_sim_fit = quantile(mean_fit, 0.5)) ## 0.5 = median, 0.9 = mean
+         mean_sim_fit = quantile(mean_fit, 0.9)) ## 0.5 = median
 
 summarized_data %>%
   ggplot(.) +
@@ -95,5 +95,16 @@ all_data %>%
   ggplot(.,aes(x=as.factor(inv_genotype),y=mean_fit)) +
   geom_boxplot()
 
-
-## group the negative fitness individuals, check if from different runs
+# Finding problems
+all_data %>%
+  mutate(fixed_fitness = case_when(inv_genotype == 2 & pop %in% c("pop1","pop2","pop4") ~ fitness - 0.1,
+                                   inv_genotype == 2 & pop %in% c("pop6","pop8","pop9") ~ fitness + 0.1,
+                                   inv_genotype == 1 & pop %in% c("pop1","pop2","pop4") ~ fitness - 0.05,
+                                   inv_genotype == 1 & pop %in% c("pop6","pop8","pop9") ~ fitness + 0.05,
+                                   TRUE ~ fitness)) %>%
+  filter(inv_genotype==2, sim_run==914) %>%
+  group_by(gen, pop, sim_run)%>%
+  summarize(mean_fitness= mean(fixed_fitness,na.rm=T)) %>%
+  ggplot(.,aes(x=gen,y=mean_fitness)) +
+  geom_smooth(method="loess") + 
+  facet_wrap(~pop)
