ce198291b1417f9725e05c1538800be22b359246
alexpinch <alexpinch@hotmail.com>
2023-04-24 18:55:07 -0700

    Update evo_wibo_plots.R

--- R_scripts/evo_wibo_plots.R
+++ R_scripts/evo_wibo_plots.R
@@ -1,6 +1,10 @@
 # Alex Pinch, last edited April 18th 2023
 # This script makes the plots used in my poster for EVO-WIBO 2023
 
+# |--------------------------------|
+# | Packages & loading SLiM output |
+# |--------------------------------|
+
 # Required packages
 library(tidyverse)
 setwd("/Users/alexpinch/GitHub/inversion_model")
@@ -19,13 +23,6 @@ for (i in 1:files) {
   la_data <- rbind(la_data, run_data)
 }
 
-# This removes the strict fitness changes coded in the SLiM model, this way we can assess fitness changes to look for build-up of deleterious mutations
-la_data <- la_data %>% mutate(fixed_fitness = case_when(inv_genotype == 2 & pop %in% c("pop1","pop2","pop4") ~ fitness - 0.1,
-                                             inv_genotype == 2 & pop %in% c("pop6","pop8","pop9") ~ fitness + 0.1,
-                                             inv_genotype == 1 & pop %in% c("pop1","pop2","pop4") ~ fitness - 0.05,
-                                             inv_genotype == 1 & pop %in% c("pop6","pop8","pop9") ~ fitness + 0.05,
-                                             TRUE ~ fitness))
-
 # Load overdominant data
 od_data <- tibble()
 files <- length(list.files("/Users/alexpinch/GitHub/inversion_model/data_041823_od/full_runs"))
@@ -40,16 +37,74 @@ for (i in 1:files) {
   od_data <- rbind(od_data, od_run_data)
 }
 
+# |--------------------------------|
+# | Preliminary data formatting    |
+# |--------------------------------|
+
+# This removes the strict fitness changes coded in the SLiM model, this way we can assess fitness changes to look for build-up of deleterious mutations
+la_data <- la_data %>% mutate(fixed_fitness = case_when(inv_genotype == 2 & pop %in% c("pop1","pop2","pop4") ~ fitness - 0.1,
+                                             inv_genotype == 2 & pop %in% c("pop6","pop8","pop9") ~ fitness + 0.1,
+                                             inv_genotype == 1 & pop %in% c("pop1","pop2","pop4") ~ fitness - 0.05,
+                                             inv_genotype == 1 & pop %in% c("pop6","pop8","pop9") ~ fitness + 0.05,
+                                             TRUE ~ fitness))
+
+# Calculate deleterious load
+la_data <- la_data %>% mutate(del_load = case_when(fixed_fitness > 0.5 ~ 1 - fixed_fitness,
+                                                   TRUE ~ fitness))
+
 # This removes the global fitness increase for all inversion-carrying heterozygotes
 od_data <- od_data %>% mutate(fixed_fitness = case_when(inv_genotype == 2 ~ fitness - 0.05,
                                                         inv_genotype == 1 ~ fitness - 0.1,
                                                         TRUE ~ fitness))
 
+# This creates a "deleterious load" variables by 
+od_data <- od_data %>% mutate(del_load = case_when(fixed_fitness > 0.5 ~ 1 - fixed_fitness,
+                                                   TRUE ~ fitness))
+
 # |-------------------|
-# | Plotting Figure 1 |
+# | Figures           |
 # |-------------------|
-# This figure plots fitness over time for each inversion genotype in the locally adaptive model.
-# Format average fitness (compare this to overdominance model)
+
+# Figure 1: Locally adaptive model deleterious load at 51,000 and 100k gen.
+la_data %>%
+  filter(!is.na(inv_genotype), gen==51000) %>% # Change gen=1e5 to compare to last generation
+  group_by(sim_run, pop, inv_genotype) %>%
+  summarize(mean_load = mean(del_load,na.rm=T)) %>%
+  group_by(sim_run,inv_genotype) %>%
+  mutate(qnt_90 = quantile(mean_load, 0.9),
+         qnt_10 = quantile(mean_load, 0.1),
+         mean_sim_load = quantile(mean_load, 0.9)) %>%  ## 0.5 = median
+  ggplot(.,aes(x=as.factor(inv_genotype),y=mean_sim_load,group=inv_genotype,color=as.factor(inv_genotype))) +
+  geom_boxplot() + geom_jitter(width = 0.2) +
+  labs(x = "Genotype", y = "Deleterial load") + 
+  scale_color_discrete(name = "Inversion Genotype")
+
+# Figure 2: Overdominant model deleterious load at 51,000 and 100k gen.
+od_data %>%
+  filter(!is.na(inv_genotype), gen==51000) %>% # Change gen=1e5 to compare to last generation
+  group_by(sim_run, pop, inv_genotype) %>%
+  summarize(mean_load = mean(del_load,na.rm=T)) %>%
+  group_by(sim_run,inv_genotype) %>%
+  mutate(qnt_90 = quantile(mean_load, 0.9),
+         qnt_10 = quantile(mean_load, 0.1),
+         mean_sim_load = quantile(mean_load, 0.9)) %>%  ## 0.5 = median
+  ggplot(.,aes(x=as.factor(inv_genotype),y=mean_sim_load,group=inv_genotype,color=as.factor(inv_genotype))) +
+  geom_boxplot() + geom_jitter(width = 0.2) +
+  labs(x = "Genotype", y = "Deleterial load") + 
+  scale_color_discrete(name = "Inversion Genotype")
+
+# |--------------------------------|
+# | Plotting supplementary figures |
+# |--------------------------------|
+
+# Supplementary figure: checking locally adaptive fitness distribution with ECDF plot
+ggplot(la_data, aes(fitness)) + stat_ecdf(geom = "point")
+
+# Supplementary figure: checking overdominant fitness distribution with ECDF plot
+ggplot(od_data, aes(fitness)) + stat_ecdf(geom = "point")
+
+# Supplementary figure: la model, fitness over time
+# Format average fitness 
 la_data %>%
   filter(!is.na(inv_genotype)) %>%
   group_by(gen, sim_run, inv_genotype) %>%
@@ -65,29 +120,7 @@ la_data %>%
   scale_color_discrete(name = "Inversion Genotype") + # Generates a second legend with a readable title
   guides(fill = "none") # Removes the original unreadable legend
 
-# |-------------------|
-# | Plotting Figure 2 |
-# |-------------------|
-# This figure plots average fitness of each run at 100,000 generations for each genotype in the overdominant model.
-# Calculate average fitness at the last generation
-la_data %>%
-  filter(!is.na(inv_genotype), gen==1e5) %>% # Change to gen=51000 to compare to first generation
-  group_by(sim_run, pop, inv_genotype) %>%
-  summarize(mean_fit = mean(fixed_fitness,na.rm=T)) %>%
-  group_by(sim_run,inv_genotype) %>%
-  mutate(qnt_90 = quantile(mean_fit, 0.9),
-         qnt_10 = quantile(mean_fit, 0.1),
-         mean_sim_fit = quantile(mean_fit, 0.9)) %>%  ## 0.5 = median
-  ggplot(.,aes(x=inv_genotype,y=mean_sim_fit,group=inv_genotype,color=inv_genotype)) +
-  geom_boxplot() + geom_jitter(width = 0.2) +
-  facet_wrap(~pop) + 
-  labs(x = "Genotype", y = "Mean fitness") + 
-  scale_color_continuous(name = "Inversion Genotype")
-
-# |-------------------|
-# | Plotting Figure 3 |
-# |-------------------|
-# This figure plots fitness over time for each inversion genotype in the overdominant model.
+# Supplementary figure: od model, fitness over time 
 # Format average fitness
 od_data %>%
   filter(!is.na(inv_genotype)) %>%
@@ -104,10 +137,7 @@ od_data %>%
   scale_color_discrete(name = "Inversion Genotype") + # Generates a second legend with a readable title
   guides(fill = "none") # Removes the original unreadable legend
 
-# |-------------------|
-# | Plotting Figure 4 |
-# |-------------------|
-# This figure plots average fitness of each run at 100,000 generations for each genotype in the overdominant model.
+# Supplementary figure: od model, genotype fitness at 100k gen. 
 # Calculate average fitness at the last generation
 od_data %>%
   filter(!is.na(inv_genotype), gen==1e5) %>% # Change to gen=51000 to compare to first generation
@@ -119,19 +149,22 @@ od_data %>%
          mean_sim_fit = quantile(mean_fit, 0.9)) %>%  ## 0.5 = median
   ggplot(.,aes(x=as.factor(inv_genotype),y=mean_sim_fit,group=inv_genotype,color=as.factor(inv_genotype))) +
   geom_boxplot() + geom_jitter(width = 0.2) +
-  facet_wrap(~pop) + 
   labs(x = "Genotype", y = "Mean fitness") + 
   scale_color_discrete(name = "Inversion Genotype")
 
-# |--------------------------------|
-# | Plotting supplementary figures |
-# |--------------------------------|
-
-# Supplementary figure: checking locally adaptive fitness distribution with ECDF plot
-ggplot(la_data, aes(fitness)) + stat_ecdf(geom = "point")
-
-# Supplementary figure: checking overdominant fitness distribution with ECDF plot
-ggplot(od_data, aes(fitness)) + stat_ecdf(geom = "point")
+# Supplementary figure: la model, genotype fitness at 100k gen.
+la_data %>%
+  filter(!is.na(inv_genotype), gen==1e5, fixed_fitness > 0.5) %>% # Change to gen=51000 to compare to first generation, removes outliers
+  group_by(sim_run, pop, inv_genotype) %>%
+  summarize(mean_fit = mean(fixed_fitness,na.rm=T)) %>%
+  group_by(sim_run,inv_genotype) %>%
+  mutate(qnt_90 = quantile(mean_fit, 0.9),
+         qnt_10 = quantile(mean_fit, 0.1),
+         mean_sim_fit = quantile(mean_fit, 0.9)) %>%  ## 0.5 = median
+  ggplot(.,aes(x=as.factor(inv_genotype),y=mean_sim_fit,group=inv_genotype,color=as.factor(inv_genotype))) +
+  geom_boxplot() + geom_jitter(width = 0.2) +
+  labs(x = "Genotype", y = "Mean fitness") + 
+  scale_color_discrete(name = "Inversion Genotype")
 
 # Supplementary figure: fitness over time for each population 
 la_data %>%
@@ -183,3 +216,5 @@ la_data %>%
   labs(x = "Genotype", y = "Inversion Frequency") + 
   scale_color_continuous(name = "Inversion Genotype")
 
+
+# REVISED PLOTS!! APRIL 24th very last minute oh my god
