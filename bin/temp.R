4b15d738666f4aab26e20c51b6268dc1f1aff8df
pinc-h <alexpinch@hotmail.com>
2024-11-27 20:07:08 -0800

    Update temp.R

--- bin/temp.R
+++ bin/temp.R
@@ -5,11 +5,11 @@ library(tidyverse)
 #   rename(gen=X1,pop=X2) %>%
 #   pivot_longer(c(-gen,-pop), names_to = "sample", values_to = "fitness")
 
-del_muts_fit <- read_delim(paste0("/Users/alexpinch/GitHub/private/inversion_model/lib/del_muts_fitness_4221439735696159844.csv"),col_names=F) %>%
+del_muts_fit <- read_delim(paste0("/Users/alexpinch/GitHub/private/inversion_model/lib/del_muts_fitness_4867507197415516269.csv"),col_names=F) %>%
   rename(gen=X1,pop=X2) %>%
   pivot_longer(c(-gen,-pop), names_to = "sample", values_to = "fitness")
 
-del_muts_genotypes <- read_delim(paste0("/Users/alexpinch/GitHub/private/inversion_model/lib/del_muts_genotypes_4221439735696159844.csv"),col_names=F) %>%
+del_muts_genotypes <- read_delim(paste0("/Users/alexpinch/GitHub/private/inversion_model/lib/del_muts_genotypes_4867507197415516269.csv"),col_names=F) %>%
   rename(gen=X1,pop=X2) %>%
   pivot_longer(c(-gen,-pop), names_to = "sample",values_to = "inv_genotype")
 
@@ -27,18 +27,19 @@ del_muts <- del_muts_fit %>%
 no_del_muts <- no_del_muts_fit %>%
   inner_join(no_del_muts_genotypes)
 
-no_del_muts %>%
-  filter(!is.na(inv_genotype), gen==199000, fitness > 0.06) %>%
+del_muts %>%
+  filter(!is.na(inv_genotype), gen==199000, fitness > 0.08) %>%
   group_by(pop, inv_genotype) %>%
   summarize(mean_fit = mean(fitness,na.rm=T)) %>%
   ggplot(.,aes(x=as.factor(inv_genotype),y=mean_fit,group=inv_genotype,color=as.factor(inv_genotype))) +
   geom_boxplot() + geom_jitter(width = 0.2) + facet_wrap(~pop) +
-  labs(x = "Genotype", y = "Fitness", title = "no del muts, gen 199,000") +
+  labs(x = "Genotype", y = "Fitness", title = "del muts, gen 199,000") +
   scale_color_discrete(name = "Inversion Genotype") + 
-  theme(text = element_text(size = 20))
+  theme(text = element_text(size = 20)) +
+  ylim(0.8,1.1)
 
 frequency <- del_muts %>%
-  filter(!is.na(inv_genotype), gen==101000) %>%
+  filter(!is.na(inv_genotype), gen==199000) %>%
   group_by(pop, inv_genotype) %>%
   summarise(count = n(), .groups = "drop") %>%
   mutate(frequency = count / 500)
@@ -46,7 +47,8 @@ frequency <- del_muts %>%
 frequency %>%
   ggplot(.,aes(x=as.factor(inv_genotype),y=frequency,group=inv_genotype,fill=as.factor(inv_genotype))) +
   geom_bar(stat = "identity", position = "dodge") +
-  labs(x = "Genotype", y = "Frequency", title = "no del muts") +
+  labs(x = "Genotype", y = "Frequency", title = "del muts") +
   scale_color_discrete(name = "Inversion Genotype") + 
-  theme(text = element_text(size = 20))
+  theme(text = element_text(size = 20)) +
+  ylim(0,1)
   
