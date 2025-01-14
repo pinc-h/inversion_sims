21abfa5e5c2fc38a55ef2495a3d7bcb0c6242681
pinc-h <alexpinch@hotmail.com>
2025-01-13 23:40:38 -0800

    Update temp.R

--- bin/temp.R
+++ bin/temp.R
@@ -5,33 +5,69 @@ library(tidyverse)
 #   rename(gen=X1,pop=X2) %>%
 #   pivot_longer(c(-gen,-pop), names_to = "sample", values_to = "fitness")
 
-del_muts_fit <- read_delim(paste0("/Users/alexpinch/GitHub/private/inversion_model/lib/del_muts_fitness_4867507197415516269.csv"),col_names=F) %>%
-  rename(gen=X1,pop=X2) %>%
-  pivot_longer(c(-gen,-pop), names_to = "sample", values_to = "fitness")
+# del_muts_fit <- read_delim(paste0("/Users/alexpinch/GitHub/private/inversion_model/lib/del_muts_fitness_4867507197415516269.csv"),col_names=F) %>%
+#   rename(gen=X1,pop=X2) %>%
+#   pivot_longer(c(-gen,-pop), names_to = "sample", values_to = "fitness")
+# 
+# del_muts_genotypes <- read_delim(paste0("/Users/alexpinch/GitHub/private/inversion_model/lib/del_muts_genotypes_4867507197415516269.csv"),col_names=F) %>%
+#   rename(gen=X1,pop=X2) %>%
+#   pivot_longer(c(-gen,-pop), names_to = "sample",values_to = "genotypes")
+# 
+# del_muts <- del_muts_fit %>%
+#   inner_join(del_muts_genotypes)
 
-del_muts_genotypes <- read_delim(paste0("/Users/alexpinch/GitHub/private/inversion_model/lib/del_muts_genotypes_4867507197415516269.csv"),col_names=F) %>%
-  rename(gen=X1,pop=X2) %>%
-  pivot_longer(c(-gen,-pop), names_to = "sample",values_to = "inv_genotype")
 
-no_del_muts_fit <- read_delim(paste0("/Users/alexpinch/GitHub/private/inversion_model/lib/no_del_muts_fitness_8227572936049252286.csv"),col_names=F) %>%
-  rename(gen=X1,pop=X2) %>%
-  pivot_longer(c(-gen,-pop), names_to = "sample", values_to = "fitness")
+lib_dir <- "/Users/alexpinch/GitHub/private/inversion_model/lib/241203"
 
-no_del_muts_genotypes <- read_delim(paste0("/Users/alexpinch/GitHub/private/inversion_model/lib/no_del_muts_genotypes_8227572936049252286.csv"),col_names=F) %>%
-  rename(gen=X1,pop=X2) %>%
-  pivot_longer(c(-gen,-pop), names_to = "sample",values_to = "inv_genotype")
+# Read all "del_muts_fitness_*" files
+del_muts_fit <- list.files(path = lib_dir, pattern = "del_muts_fitness_.*\\.csv", full.names = TRUE) %>%
+  map_df(~ {
+    file_name <- basename(.x)  # Get the file name without the path
+    seed <- gsub(".*del_muts_fitness_(\\d+).*", "\\1", file_name)  # Extract the seed number
+    read_delim(.x, col_names = FALSE) %>%
+      rename(gen = X1, pop = X2) %>%
+      pivot_longer(cols = -c(gen, pop), names_to = "sample", values_to = "fitness") %>%
+      mutate(seed = seed) 
+  })
 
+# Read all "del_muts_genotype_*" files
+del_muts_genotype <- list.files(path = lib_dir, pattern = "del_muts_genotypes_.*\\.csv", full.names = TRUE) %>%
+  map_df(~ {
+    file_name <- basename(.x)  # Get the file name without the path
+    seed <- gsub(".*del_muts_genotypes_(\\d+).*", "\\1", file_name)  # Extract the seed number
+    read_delim(.x, col_names = FALSE) %>%
+      rename(gen = X1, pop = X2) %>%
+      pivot_longer(cols = -c(gen, pop), names_to = "sample", values_to = "genotypes") %>%
+      mutate(seed = seed) 
+  })
+
+# This appends the population to the sample ID. This helps me keep them straight as now each individual sample is unique, no getting pops mixed up
+del_muts_fit <- del_muts_fit %>%
+  mutate(sample = paste0(gsub("\\D", "", pop), "_", sample))
+del_muts_genotype <- del_muts_genotype %>%
+  mutate(sample = paste0(gsub("\\D", "", pop), "_", sample))
+
+# Merge the datasets
 del_muts <- del_muts_fit %>%
-  inner_join(del_muts_genotypes)
+  inner_join(del_muts_genotype, by = c("gen", "pop", "sample", "seed"))
 
-no_del_muts <- no_del_muts_fit %>%
-  inner_join(no_del_muts_genotypes)
+
+# no_del_muts_fit <- read_delim(paste0("/Users/alexpinch/GitHub/private/inversion_model/lib/no_del_muts_fitness_8227572936049252286.csv"),col_names=F) %>%
+#   rename(gen=X1,pop=X2) %>%
+#   pivot_longer(c(-gen,-pop), names_to = "sample", values_to = "fitness")
+# 
+# no_del_muts_genotypes <- read_delim(paste0("/Users/alexpinch/GitHub/private/inversion_model/lib/no_del_muts_genotypes_8227572936049252286.csv"),col_names=F) %>%
+#   rename(gen=X1,pop=X2) %>%
+#   pivot_longer(c(-gen,-pop), names_to = "sample",values_to = "genotypes")
+# 
+# no_del_muts <- no_del_muts_fit %>%
+#   inner_join(no_del_muts_genotypes)
 
 del_muts %>%
-  filter(!is.na(inv_genotype), gen==199000, fitness > 0.08) %>%
-  group_by(pop, inv_genotype) %>%
+  filter(!is.na(genotypes), gen==101000, fitness > 0.08) %>%
+  group_by(pop, genotypes) %>%
   summarize(mean_fit = mean(fitness,na.rm=T)) %>%
-  ggplot(.,aes(x=as.factor(inv_genotype),y=mean_fit,group=inv_genotype,color=as.factor(inv_genotype))) +
+  ggplot(.,aes(x=as.factor(genotypes),y=mean_fit,group=genotypes,color=as.factor(genotypes))) +
   geom_boxplot() + geom_jitter(width = 0.2) + facet_wrap(~pop) +
   labs(x = "Genotype", y = "Fitness", title = "del muts, gen 199,000") +
   scale_color_discrete(name = "Inversion Genotype") + 
@@ -39,16 +75,42 @@ del_muts %>%
   ylim(0.8,1.1)
 
 frequency <- del_muts %>%
-  filter(!is.na(inv_genotype), gen==199000) %>%
-  group_by(pop, inv_genotype) %>%
-  summarise(count = n(), .groups = "drop") %>%
-  mutate(frequency = count / 500)
+  filter(!is.na(genotypes), gen==199000) %>%
+  group_by(pop, genotypes) %>%
+  summarise(count = n(), .groups = "drop")
 
 frequency %>%
-  ggplot(.,aes(x=as.factor(inv_genotype),y=frequency,group=inv_genotype,fill=as.factor(inv_genotype))) +
+  ggplot(.,aes(x=as.factor(genotypes),y=count,group=genotypes,fill=as.factor(genotypes))) +
   geom_bar(stat = "identity", position = "dodge") +
-  labs(x = "Genotype", y = "Frequency", title = "del muts") +
+  labs(x = "Genotype", y = "Number", title = "del muts") +
   scale_color_discrete(name = "Inversion Genotype") + 
   theme(text = element_text(size = 20)) +
-  ylim(0,1)
+  ylim(0,40000)
+
+# "Deleterial load"
+# This removes the strict fitness changes coded in the SLiM model, this way we can assess fitness changes to look for build-up of deleterious mutations
+del_muts <- del_muts %>% mutate(fixed_fitness = case_when(genotypes == 2 & pop %in% c("pop1","pop2","pop4") ~ fitness - 0.05,
+                                                        genotypes == 2 & pop %in% c("pop6","pop8","pop9") ~ fitness + 0.05,
+                                                        genotypes == 1 & pop %in% c("pop1","pop2","pop4") ~ fitness - 0.025,
+                                                        genotypes == 1 & pop %in% c("pop6","pop8","pop9") ~ fitness + 0.025,
+                                                        TRUE ~ fitness))
+del_muts <- del_muts %>% mutate(del_load = case_when(fixed_fitness > 0.5 ~ 1 - fixed_fitness,
+                                                   TRUE ~ fitness))
+
+
+del_muts %>%
+  filter(!is.na(genotypes), gen==103000, del_load < 0.5) %>% # Change gen=1e5 to compare to last generation
+  group_by(seed, pop, del_load, genotypes) %>%
+  summarize(mean_load = mean(del_load,na.rm=T)) %>%
+  group_by(seed, genotypes) %>%
+  mutate(qnt_90 = quantile(mean_load, 0.9),
+         qnt_10 = quantile(mean_load, 0.1),
+         mean_seed_load = quantile(mean_load, 0.9)) %>%  ## 0.5 = median
+  ggplot(.,aes(x=as.factor(genotypes),y=mean_seed_load,group=genotypes,color=as.factor(genotypes))) +
+  geom_boxplot() + geom_jitter(width = 0.2) +
+  labs(x = "Genotype", y = "Deleterial load", title="generation 103,000") +
+  scale_color_discrete(name = "Inversion Genotype") + 
+  ylim(0, 0.3) +
+  theme(text = element_text(size = 20)) 
+  
   
